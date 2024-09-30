fd46fc18680978543a1d071c3b2b2493d75381be
Isabela Melleis <isabela.zmelleis@gmail.com>
2024-09-30 11:41:07 -0300

    Atualizando arquivos de update e index
    Co-Authored-By: Lorenzo Broch <160625132+LorenzoBroch@users.noreply.github.com>

--- aula_crud_em_dupla/index.php
+++ aula_crud_em_dupla/index.php
@@ -14,43 +14,38 @@
     <p>
         Abaixo, adicione todas as aulas que já aconteceram e a sua duração.
     </p>
-    <form action="index.php" method="$_POST">
+    <form action="index.php" method="POST">
         <label for="nome_aula">Aula:</label>
         <select name="nome_aula" required>
             <option selected disabled>Selecione</option>
-            <?php
-            include 'db_connect.php';
-
-            $sql_aulas = "SELECT id_aula, nome_aula FROM aula";
-            $result_aulas = $conn -> query($sql_aulas);
-
-            if ($result_aulas ->  num_rows > 0) {
-                while($row = $result_aulas -> fetch_assoc()) {
-                    echo "<option value='{$row['id_aula']}'>{$row['nome_aula']}</option>";
-                }
-            } else {
-                echo "<option disabled>Nenhuma matéria encontrada</option>";
-            }
             
+            <?php
+            
+                if ($result_aulas ->  num_rows > 0) {
+                    while($row = $result_aulas -> fetch_assoc()) {
+                        echo "<option value='{$row['id_aula']}'>{$row['nome_aula']}</option>";
+                    }
+                } else {
+                    echo "<option disabled>Nenhuma matéria encontrada</option>";
+                }
+                
             ?>
+            
         </select>
 
         <label for="nome_professor">Professor:</label>
         <select name="nome_professor" required>
             <option selected disabled>Selecione</option>
             <?php
-            include 'db_connect.php';
 
-            $sql_professores = "SELECT id_professor, nome_professor FROM professor";
-            $result_professores = $conn -> query($sql_professores);
-
-            if ($result_professores -> num_rows > 0) {
-                while($row = $result_professores -> fetch_assoc()) {
-                    echo "<option value='{$row['id_professor']}'>{$row['nome_professor']}</option>";
+                if ($result_professores -> num_rows > 0) {
+                    while($row = $result_professores -> fetch_assoc()) {
+                        echo "<option value='{$row['id_professor']}'>{$row['nome_professor']}</option>";
+                    }
+                } else {
+                    echo "<option disabled>Nenhum professor encontrada</option>";
                 }
-            } else {
-                echo "<option disabled>Nenhum professor encontrada</option>";
-            }
+
             ?>
         </select>
 
@@ -61,59 +56,66 @@
             Adicionar
         </button>
     </form>
+    <br>
 </body>
 </html>
 
-<?php 
-include 'db_connect.php';
+<?php
+    include 'db_connect.php';
 
-if(isset($_POST['adicionar_aula_historico'])) {
-    $duracao_aula = $_POST['duracao_aula'];
-    $fk_professor = $_POST['professor'];
-    $fk_aula = $_POST['aula'];
+    $sql_aulas = "SELECT id_aula, nome_aula FROM aula";
+    $result_aulas = $conn -> query($sql_aulas);
 
-    $sql_create = "INSERT INTO diario (duracao_aula) VALUE ('$duracao_aula)";
+    $sql_professores = "SELECT id_professor, nome_professor FROM professor";
+    $result_professores = $conn -> query($sql_professores);
 
-    if($conn -> query($sql_create) === TRUE){
-        echo "Novo pedido feito com sucesso!";
-    }else{
-        echo "Erro:". $sql."<br>".$conn -> error;
+    if(isset($_POST['adicionar_aula_historico'])) {
+        $fk_aula = $_POST['nome_aula'];
+        $fk_professor = $_POST['nome_professor'];
+        $duracao_aula = $_POST['duracao_aula'];
+    
+        $sql_create = "INSERT INTO diario (duracao_aula, fk_professor, fk_aula) VALUE ('$duracao_aula','$fk_professor','$fk_aula')";
+    
+        if($conn -> query($sql_create) === TRUE){
+            echo "Novo cadastro feito com sucesso!";
+        }else{
+            echo "Erro:". $sql."<br>".$conn -> error;
+        }
     }
-}
 
-/*$sql = "SELECT horario_aula, nome_aula, nome_professor, numero_sala, duracao_aula FROM diario INNER JOIN professor on fk_professor = id_professor INNER JOIN aula on fk_aula = id_aula;";
+    $sql = "SELECT id_diario, horario_aula, nome_aula, nome_professor, numero_sala, duracao_aula FROM diario INNER JOIN professor on fk_professor = id_professor INNER JOIN aula on fk_aula = id_aula;";
 
-$result = $conn -> query($sql);
+    $result = $conn -> query($sql);
 
-if ($result -> num_rows > 0) {
-    echo"<table_border='1'>
-    <tr>
-            <th> ID </th>
-            <th> Horário da aula </th>
-            <th> Aula </th>
-            <th> Professor </th>
-            <th> Número da sala </th>
-            <th> Duração da aula </th>
-    </tr>";
-    while($row = $result -> fetch_assoc()){
-        echo "<tr>
-                <td> {$row['id_diario']} </td>
-                <td> {$row['horario_aula']} </td>
-                <td> {$row['nome_aula']} </td>
-                <td> {$row['nome_professor']} </td>
-                <td> {$row['numero_sala']} </td>
-                <td> {$row['duracao_aula']} </td>
-                <td>
-                    <a href='update.php?id={$row['id']}'>Editar</a>
-                </td>
-            </tr>"; 
-}
-echo "</table>";
-}
-else{
-    echo "Nenhum registro encontrado.";
-}
-
-$conn -> close();
-*/
+    if ($result -> num_rows > 0) {
+        echo"<table border='1'>
+        <tr>
+                <th> ID </th>
+                <th> Horário da aula </th>
+                <th> Aula </th>
+                <th> Professor </th>
+                <th> Número da sala </th>
+                <th> Duração da aula </th>
+        </tr>";
+        while($row = $result -> fetch_assoc()){
+            echo "<tr>
+                    <td> {$row['id_diario']} </td>
+                    <td> {$row['horario_aula']} </td>
+                    <td> {$row['nome_aula']} </td>
+                    <td> {$row['nome_professor']} </td>
+                    <td> {$row['numero_sala']} </td>
+                    <td> {$row['duracao_aula']} </td>
+                    <td>
+                        <a href='update.php'><button>Editar</button></a>
+                        <button>
+                            Excluir
+                        </button>
+                    </td>
+                </tr>"; 
+    }
+        echo "</table>";
+    } else{
+            echo "Nenhum registro encontrado.";
+    }
+    $conn -> close();
 ?>
--- aula_crud_em_dupla/update.php
+++ aula_crud_em_dupla/update.php
@@ -1,3 +1,48 @@
-<?php 
+<?php
 
+include 'db_connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $fk_professor = $_POST['nome_professor'];
+    $fk_aula = $_POST['nome_aula'];
+    $duracao_aula = $_POST['duracao_aula'];
+
+    $sql = "UPDATE user SET nome_aula = '$fk_aula', fk_professor = '$nome_professor', duracao_aula = '$duracao_aula'";
+    if ($conn->query($sql) === TRUE) {
+        echo "Atualização realidada com sucesso";
+    } else {
+        echo "Erro: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UPDATE do Diário</title>
+</head>
+
+<body>
+    <h2>Alterar professor</h2>
+    <form method="POST" action="update.php">
+        <label>Novo nome: </label>
+        <input type="text" name="nome_professor" required>
+    </form>
+    <h2>Alterar aula</h2>
+    <form method="POST" action="update.php">
+        <label>Nova aula: </label>
+        <input type="text" name="nome_aula" required>
+    </form>
+    <h2>Alterar Duração da Aula</h2>
+    <form method="POST" action="update.php">
+        <label>Nova Duração: </label>
+        <input type="text" name="duracao_aula" required>
+    </form>
+    <button type="submit" name="adicionar_aula_historico"> Atualizar </button>
+</body>
+
+</html>
